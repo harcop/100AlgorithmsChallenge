1f755c276a995c11de87ee4ebd3ac5302eab90d5
harcop <harcopdrew@gmail.com>
2021-02-20 06:41:16 +0100

    absoluteValueSumMinimazation

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,9 +1,19 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+    let min = Infinity;
+    let min_a = 0;
+    for (let _a of a) {
+        let _min = 0;
+        for(let _b of a) {
+            _min  += Math.abs(_b - _a);
+        }
+        if (_min < min) {
+            min = _min;
+            min_a = _a;
+        }
+    }
+    return min_a
 }
-const a: number[] = [2, 4, 7]
-console.log(a.length/2 - 1)
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
